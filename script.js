c57229d2e6a3fc5698ddb09ef3bedfb260248ba4
robert-dev <kaleft212@gmail.com>
2021-12-29 14:23:52 -0400

    finalizado

---
+++ script.js
@@ -0,0 +1,117 @@
+const musicContainer = document.querySelector(".music-container");
+
+const playBtn = document.querySelector("#play");
+
+const prevBtn = document.querySelector("#prev");
+
+const nextBtn = document.querySelector("#next");
+
+const audio = document.querySelector("#audio");
+
+const progress = document.querySelector(".progress");
+
+const progressContainer = document.querySelector(".progress-container");
+
+const title = document.querySelector("#title");
+
+const cover = document.querySelector("#cover");
+
+// Titulos //
+const songs = ["Do-Do-Do", "Rhinestone-Eyes", "Everytime"];
+
+// Seguirle la pista a las canciones //
+let songIndex = 2;
+
+// Cargar la información de la canción al DOM //
+loadSong(songs[songIndex]);
+
+//Actualizar detalles de la canción //
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `img/${song}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-play");
+
+  playBtn.querySelector("i.fa-solid").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+
+  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
+
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+
+  const clickX = e.offsetX;
+
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners //
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Cambiar musica //
+prevBtn.addEventListener("click", prevSong);
+
+nextBtn.addEventListener("click", nextSong);
+// Cambiar musica //
+
+audio.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
